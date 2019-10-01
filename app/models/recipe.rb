c8fda1dd997f1078731c544327e0305db03bea32
Laura Meireles <laura.meireles@revelo.com.br>
2019-10-01 13:48:38 -0300

    my first commit

---
+++ app/models/recipe.rb
@@ -0,0 +1,9 @@
+class Recipe < ApplicationRecord
+
+  validates :title, :recipe_type, :cuisine, :difficulty, :cook_time, 
+            :ingredients, :cook_method, presence: true
+
+  def cook_time_minutes
+    "#{cook_time} minutos"
+  end
+end
